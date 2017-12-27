962d656294bba6dd3faa3eff6ccd13b399683db5
D0tNet4Fun <D0tNet4Fun@users.noreply.github.com>
2017-12-27 12:12:40 +0200

    #1 Added new test framework. This is now loaded in the test project instead of the default Xunit test framework.

---
+++ Automation.TestFramework/Framework.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Automation.TestFramework
+{
+    public class Framework : XunitTestFramework
+    {
+        public Framework(IMessageSink messageSink)
+            : base(messageSink)
+        {
+            messageSink.OnMessage(new DiagnosticMessage("Hello world"));
+        }
+    }
+}
