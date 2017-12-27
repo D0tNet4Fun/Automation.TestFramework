3c976263a827ebef1de7bb49c85f8b95df4d5970
D0tNet4Fun <D0tNet4Fun@users.noreply.github.com>
2017-12-27 13:34:21 +0200

    #2 Added attributes to define the structure of a test case: summary, precondition, test step input, test step expected result.

---
+++ Automation.TestFramework/SummaryAttribute.cs
@@ -0,0 +1,17 @@
+﻿namespace Automation.TestFramework
+{
+    /// <summary>
+    /// Identifies a test method as the summary of a test case.
+    /// </summary>
+    public class SummaryAttribute : TestCaseComponentAttribute
+    {
+        public SummaryAttribute(string description)
+            : base(0, description)
+        {
+
+        }
+
+        protected override string GetDisplayName(string description)
+            => description;
+    }
+}
---
+++ Automation.TestFramework/TestCaseComponentAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Xunit;
+
+namespace Automation.TestFramework
+{
+    public abstract class TestCaseComponentAttribute : FactAttribute
+    {
+        public int Order { get; }
+        private readonly string _description;
+
+        protected TestCaseComponentAttribute(int order, string description)
+        {
+            if (order < 0)
+                throw new ArgumentOutOfRangeException(nameof(order), "Order must be positive");
+
+            if (string.IsNullOrEmpty(description))
+                throw new ArgumentException("Description cannot be null or empty.", nameof(description));
+
+            Order = order;
+            _description = description;
+        }
+
+        public override string DisplayName
+        {
+            get => GetDisplayName(_description);
+            set => throw new NotSupportedException("Use ctor to set description"); // todo
+        }
+
+        protected abstract string GetDisplayName(string description);
+    }
+}
