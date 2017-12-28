1f66232245808e8a060f56204669da4a4de62bf7
D0tNet4Fun <D0tNet4Fun@users.noreply.github.com>
2017-12-28 18:53:15 +0200

    #10 Added AssemblyFixtureAttribute (copied from xunit samples)

---
+++ Automation.TestFramework/AssemblyFixtureAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Automation.TestFramework
+{
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class AssemblyFixtureAttribute : Attribute
+    {
+        public AssemblyFixtureAttribute(Type fixtureType)
+        {
+            FixtureType = fixtureType;
+        }
+
+        public Type FixtureType { get; }
+    }
+}
