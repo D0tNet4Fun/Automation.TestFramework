af63a21cceff6b1dabca3002374de16f46aae485
D0tNet4Fun <D0tNet4Fun@users.noreply.github.com>
2017-12-29 13:46:25 +0200

    #14 Do not discover test cases without Summary or with more than one Summary.

---
+++ Automation.TestFramework.Tests/TestCaseWithInvalidSummary.cs
@@ -0,0 +1,24 @@
+﻿namespace Automation.TestFramework.Tests
+{
+    [TestCase("no summary")]
+    public class TestCaseWithNoSummary
+    {
+
+    }
+
+    [TestCase("too many summaries")]
+    public class TestCaseWithMoreSummaries
+    {
+        [Summary]
+        public void X()
+        {
+
+        }
+
+        [Summary]
+        public void Y()
+        {
+
+        }
+    }
+}
--- Automation.TestFramework/Discovery/FrameworkDiscoverer.cs
+++ Automation.TestFramework/Discovery/FrameworkDiscoverer.cs
@@ -19,5 +19,24 @@ namespace Automation.TestFramework.Discovery
                 DiagnosticMessageSink.OnMessage(new DiagnosticMessage($"Class {type.Name} is not a test case."));
             return isTestCase;
         }
+
+        protected override bool FindTestsForType(ITestClass testClass, bool includeSourceInformation, IMessageBus messageBus, ITestFrameworkDiscoveryOptions discoveryOptions)
+        {
+            var type = testClass.Class;
+            var summaryMethods = type.GetMethods(includePrivateMethods: false).Where(m => m.GetCustomAttributes(typeof(SummaryAttribute)).Any()).ToList();
+            if (summaryMethods.Count == 0)
+            {
+                DiagnosticMessageSink.OnMessage(new DiagnosticMessage($"Test case class {type.Name} does not have a Summary method. This will not be discovered."));
+                return true;
+            }
+
+            if (summaryMethods.Count > 1)
+            {
+                DiagnosticMessageSink.OnMessage(new DiagnosticMessage($"Test case class {type.Name} has more than one Summary methods. This will not be discovered."));
+                return true;
+            }
+
+            return base.FindTestsForType(testClass, includeSourceInformation, messageBus, discoveryOptions);
+        }
     }
 }
