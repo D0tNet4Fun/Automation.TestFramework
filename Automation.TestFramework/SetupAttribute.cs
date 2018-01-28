1d6e2a6653532494c3db16d244b32e840de96a4e
D0tNet4Fun <D0tNet4Fun@users.noreply.github.com>
2018-01-28 10:00:49 +0200

    #24 Added XML documentation

--- Automation.TestFramework/SetupAttribute.cs
+++ Automation.TestFramework/SetupAttribute.cs
@@ -5,11 +5,17 @@
     /// </summary>
     public class SetupAttribute : TestCaseComponentAttribute
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SetupAttribute"/> class.
+        /// </summary>
+        /// <param name="order">The order in which the target test method needs to be executed.</param>
+        /// <param name="description">The user friendly description of the test method.</param>
         public SetupAttribute(int order = 1, string description = null)
             : base(order, description)
         {
         }
 
+        /// <inheritdoc />
         protected override string GetDisplayName(string description)
             => $"[Setup] {Order}. {description}";
     }
