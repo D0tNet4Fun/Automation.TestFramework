4b64c9b6950398b89d34b0f04191a0b79eaf3bad
D0tNet4Fun <D0tNet4Fun@users.noreply.github.com>
2024-09-24 19:16:41 +0300

    #50 Resolve ITestOutputHelper

---
+++ Automation.TestFramework.Tests/AsyncWithOutputHelper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Abstractions;
+
+namespace Automation.TestFramework.Tests
+{
+    [TestCase(nameof(AsyncWithOutputHelper))]
+    public class AsyncWithOutputHelper(ITestOutputHelper testOutputHelper)
+    {
+        [Summary]
+        public void Summary()
+        {
+
+        }
+
+        [Input]
+        private Task Input1()
+        {
+            testOutputHelper.WriteLine(nameof(Input1));
+            return Task.CompletedTask;
+        }
+
+        [Input(2)]
+        private Task Input2()
+        {
+            testOutputHelper.WriteLine(nameof(Input2));
+            return Task.CompletedTask;
+        }
+    }
+}
